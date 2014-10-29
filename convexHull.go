51e5d6155f0c112c63c92c6c255c78cae88294e6
clgdante <clgdante@outlook.com>
2014-10-29 18:26:12 +0100

    First implementation of algorithm. Need better checkPoint.

--- convexHull.go
+++ convexHull.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
 type Point struct {
-	X, Y int
+	X, Y float64
 }
 
 type ByX []Point
@@ -45,7 +46,73 @@ func main() {
 
 	fmt.Println(points)
 
-	p1 := points[len(points)-1]
+	length := len(points)
 
-	p1origin := p1
+	currentPoint := points[length-1]
+	originPoint := points[length-1]
+
+	points = points[:(length - 1)]
+
+	fmt.Println(points)
+
+	var a, c float64
+	results := []Point{
+		originPoint,
+	}
+	atOrigin := false
+
+	//TEST
+	/*alpha := math.Pi/2 - 0.01
+	a = math.Tan(alpha)
+	c = currentPoint.Y + a*currentPoint.X
+
+	fmt.Println(a)
+	fmt.Println()
+	fmt.Println(c)
+	fmt.Println()
+
+	for _, p := range points {
+		fmt.Println(distance(a, c, p))
+
+		if checkPoint(a, c, p) {
+			fmt.Println(p)
+		}
+	}*/
+
+	for alpha := math.Pi/2 - 0.1; alpha > -math.Pi/2+0.1; alpha -= 0.02 {
+		a = -math.Tan(alpha)
+		c = currentPoint.Y + a*currentPoint.X
+		for i, p := range points {
+			if checkPoint(a, c, p) {
+				currentPoint = p
+				if currentPoint == originPoint {
+					atOrigin = true
+				}
+				points = append(points[:i], points[i+1:]...)
+				results = append(results, p)
+
+				fmt.Println(a)
+				fmt.Println()
+				fmt.Println(c)
+				fmt.Println()
+				fmt.Println()
+
+				break
+			}
+		}
+		if atOrigin {
+			break
+		}
+	}
+
+	fmt.Println(results)
+
+}
+
+func distance(a, c float64, p Point) float64 {
+	return math.Abs((a*p.X + p.Y - c) / math.Sqrt(a*a+1))
+}
+
+func checkPoint(a, c float64, p Point) bool {
+	return distance(a, c, p) <= 0.7
 }

4c2fa834aa637b0a2544870ffc3c359b15963348
J0-nas <jonas@ottermoor.de>
2014-11-03 17:38:51 +0100

    added printing of convex hull (with error)

--- convexHull.go
+++ convexHull.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/J0-nas/drawGraph"
+	"log"
 	"math"
 	"sort"
 )
@@ -32,41 +33,37 @@ func main() {
 	fmt.Println("Test output!")
 
 	points := []Point{
-		{1, 2},
-		{8, 2},
-		{6, 3},
-		{3, 5},
-		{7, 7},
-		{7, 1},
-		{4, 9},
+		{10, 20},
+		{80, 20},
+		{60, 30},
+		{30, 50},
+		{70, 70},
+		{70, 10},
+		{40, 90},
 	}
 
 	convexHull := buildConvexHull(points)
 	fmt.Println(convexHull)
 
 	d := drawGraph.Instance()
-	d.NewImage(200, 200)
-	d.AddPoint(50.0, 100.0)
+	d.NewImage(100, 100)
+	for j := 0; j < len(convexHull)-1; j++ {
+		if j == len(convexHull)-1 {
+			d.AddLine(convexHull[0].X, convexHull[0].Y, convexHull[j].X, convexHull[j].Y)
+		} else {
+			d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[j+1].X, convexHull[j+1].Y)
+		}
+	}
+	for j := 0; j < len(points)-1; j++ {
+		err := d.AddPoint(convexHull[j].X, convexHull[j].Y)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	//d.AddPoint(50.0, 100.0)
+	//d.AddLine(50.0, 100.0, 100.0, 100.0)
 	d.SaveImage("test")
 
-	//d.PrintSomething()
-	//TEST
-	/*alpha := math.Pi/2 - 0.01
-	a = math.Tan(alpha)
-	c = currentPoint.Y + a*currentPoint.X
-
-	fmt.Println(a)
-	fmt.Println()
-	fmt.Println(c)
-	fmt.Println()
-
-	for _, p := range points {
-		fmt.Println(distance(a, c, p))
-
-		if checkPoint(a, c, p) {
-			fmt.Println(p)
-		}
-	}*/
 }
 
 func buildConvexHull(points []Point) []Point {
@@ -128,3 +125,22 @@ func distance(a, c float64, p Point) float64 {
 func checkPoint(a, c float64, p Point) bool {
 	return distance(a, c, p) <= 0.7
 }
+
+//d.PrintSomething()
+//TEST
+/*alpha := math.Pi/2 - 0.01
+a = math.Tan(alpha)
+c = currentPoint.Y + a*currentPoint.X
+
+fmt.Println(a)
+fmt.Println()
+fmt.Println(c)
+fmt.Println()
+
+for _, p := range points {
+	fmt.Println(distance(a, c, p))
+
+	if checkPoint(a, c, p) {
+		fmt.Println(p)
+	}
+}*/

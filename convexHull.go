7e4937143d32657981d7d4f70a6362e3f79540be
J0-nas <jonas@ottermoor.de>
2014-11-04 13:01:51 +0100

    started second algorithm

--- convexHull.go
+++ convexHull.go
@@ -5,6 +5,8 @@ import (
 	"github.com/J0-nas/drawGraph"
 	"log"
 	"math"
+	"math/rand"
+	"os"
 	"sort"
 )
 
@@ -19,7 +21,10 @@ func (a ByX) Len() int {
 }
 
 func (a ByX) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
+	pTemp := a[i]
+	a[i] = a[j]
+	a[j] = pTemp
+	//	, a[j] = a[j], a[i]
 }
 
 func (a ByX) Less(i, j int) bool {
@@ -32,7 +37,15 @@ func (a ByX) Less(i, j int) bool {
 func main() {
 	fmt.Println("Test output!")
 
-	points := []Point{
+	var points []Point
+	height := 1000.0
+	width := 1000.0
+
+	for start := 0; start < 20; start++ {
+		points = append(points, Point{rand.Float64() * width, rand.Float64() * height})
+	}
+
+	/*points := []Point{
 		{10, 20},
 		{80, 20},
 		{60, 30},
@@ -40,24 +53,33 @@ func main() {
 		{70, 70},
 		{70, 10},
 		{40, 90},
-	}
-
-	convexHull := buildConvexHull(points)
-	fmt.Println(convexHull)
-
+	}*/
 	d := drawGraph.Instance()
-	d.NewImage(100, 100)
-	for j := 0; j < len(convexHull)-1; j++ {
-		if j == len(convexHull)-1 {
-			d.AddLine(convexHull[0].X, convexHull[0].Y, convexHull[j].X, convexHull[j].Y)
-		} else {
-			d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[j+1].X, convexHull[j+1].Y)
-		}
-	}
-	for j := 0; j < len(points)-1; j++ {
-		err := d.AddPoint(convexHull[j].X, convexHull[j].Y)
+	d.NewImage(height, width)
+	for i := range points {
+		err := d.AddPoint(points[i].X, points[i].Y)
 		if err != nil {
 			log.Println(err)
+		}
+	}
+
+	convexHull := buildConvexHull_2(points)
+
+	if len(convexHull) <= 2 {
+		log.Println("Too few points in convex hull")
+		os.Exit(1)
+	}
+	for j := 0; j <= len(convexHull)-1; j++ {
+		if j == len(convexHull)-1 {
+			err := d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[0].X, convexHull[0].Y)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			err := d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[j+1].X, convexHull[j+1].Y)
+			if err != nil {
+				log.Println(err)
+			}
 		}
 	}
 	//d.AddPoint(50.0, 100.0)
@@ -66,12 +88,41 @@ func main() {
 
 }
 
-func buildConvexHull(points []Point) []Point {
-	fmt.Println(points)
+func buildConvexHull_2(points []Point) []Point {
+	sort.Sort(ByX(points))
+	currentPoint := points[0]
+	originPoint := points[0]
+	points = points[1:]
+
+	results := []Point{
+		originPoint,
+	}
+	maxAngle := 0.0
+	var maxPos int
+	for {
+		for i, p := range points {
+			a := math.Atan((p.Y - currentPoint.Y) / (p.X - currentPoint.X))
+			if a > maxAngle {
+				maxAngle = a
+				maxPos = i
+			}
+		}
+		results = append(results, points[maxPos])
+		currentPoint = points[maxPos]
+		points = append(points[:maxPos], points[maxPos+1:]...)
+		if currentPoint.Equals(originPoint) {
+			break
+		}
+	}
+	return results
+}
+
+func buildConvexHull_1(points []Point) []Point {
+	//fmt.Println(points)
 
 	sort.Sort(ByX(points))
 
-	fmt.Println(points)
+	//fmt.Println(points)
 
 	length := len(points)
 
@@ -80,7 +131,7 @@ func buildConvexHull(points []Point) []Point {
 
 	points = points[:(length - 1)]
 
-	fmt.Println(points)
+	//fmt.Println(points)
 
 	var a, c float64
 	results := []Point{
@@ -88,7 +139,7 @@ func buildConvexHull(points []Point) []Point {
 	}
 	atOrigin := false
 
-	for alpha := math.Pi/2 - 0.1; alpha > -math.Pi/2+0.1; alpha -= 0.02 {
+	for alpha := math.Pi/2 - 0.001; alpha > -(math.Pi + math.Pi/2 + 0.001); alpha -= 0.001 {
 		a = -math.Tan(alpha)
 		c = currentPoint.Y + a*currentPoint.X
 		for i, p := range points {
@@ -98,6 +149,7 @@ func buildConvexHull(points []Point) []Point {
 					atOrigin = true
 				}
 				points = append(points[:i], points[i+1:]...)
+				fmt.Println("Added point... ", p)
 				results = append(results, p)
 
 				fmt.Println(a)
@@ -113,7 +165,7 @@ func buildConvexHull(points []Point) []Point {
 			break
 		}
 	}
-
+	fmt.Print("Convec Hull: ")
 	fmt.Println(results)
 	return results
 }
@@ -123,7 +175,11 @@ func distance(a, c float64, p Point) float64 {
 }
 
 func checkPoint(a, c float64, p Point) bool {
-	return distance(a, c, p) <= 0.7
+	return distance(a, c, p) <= 0.2
+}
+
+func (p *Point) Equals(q Point) bool {
+	return p.X == q.X && p.Y == q.Y
 }
 
 //d.PrintSomething()

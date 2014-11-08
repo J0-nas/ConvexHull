8fa86ac8be788511b45a56e470baa475c59aa102
J0-nas <jonas@ottermoor.de>
2014-11-08 20:48:53 +0100

    added second algorithm

--- convexHull.go
+++ convexHull.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/J0-nas/drawGraph"
 	"log"
@@ -8,6 +9,8 @@ import (
 	"math/rand"
 	"os"
 	"sort"
+	"strconv"
+	"time"
 )
 
 type Point struct {
@@ -35,27 +38,26 @@ func (a ByX) Less(i, j int) bool {
 }
 
 func main() {
-	fmt.Println("Test output!")
-
-	var points []Point
 	height := 1000.0
 	width := 1000.0
 
-	for start := 0; start < 20; start++ {
+	for i := 0; i <= 10000; i++ {
+		convex("test_"+strconv.Itoa(i), height, width)
+	}
+}
+
+func convex(name string, height, width float64) {
+	var points []Point
+
+	seed := time.Now().UTC().UnixNano() % 128
+	rand.Seed(seed)
+	//fmt.Println("seed: ", seed)
+
+	for start := 0; start < int(rand.Float64()*1000)+3; start++ {
 		points = append(points, Point{rand.Float64() * width, rand.Float64() * height})
 	}
 
-	/*points := []Point{
-		{10, 20},
-		{80, 20},
-		{60, 30},
-		{30, 50},
-		{70, 70},
-		{70, 10},
-		{40, 90},
-	}*/
-	d := drawGraph.Instance()
-	d.NewImage(height, width)
+	d := drawGraph.NewImage(height, width)
 	for i := range points {
 		err := d.AddPoint(points[i].X, points[i].Y)
 		if err != nil {
@@ -63,58 +65,125 @@ func main() {
 		}
 	}
 
-	convexHull := buildConvexHull_2(points)
+	convexHull, err := buildConvexHull_2(points)
+	//convexHull := buildConvexHull_2(points)
+	if err != nil {
+		log.Println(err)
+	}
 
 	if len(convexHull) <= 2 {
 		log.Println("Too few points in convex hull")
 		os.Exit(1)
 	}
-	for j := 0; j <= len(convexHull)-1; j++ {
-		if j == len(convexHull)-1 {
-			err := d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[0].X, convexHull[0].Y)
-			if err != nil {
-				log.Println(err)
-			}
-		} else {
-			err := d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[j+1].X, convexHull[j+1].Y)
-			if err != nil {
-				log.Println(err)
-			}
+	for j := 0; j <= len(convexHull)-2; j++ {
+		err := d.AddLine(convexHull[j].X, convexHull[j].Y, convexHull[j+1].X, convexHull[j+1].Y)
+		if err != nil {
+			fmt.Println("\n\nError!\n\n")
+			log.Println(err)
 		}
 	}
-	//d.AddPoint(50.0, 100.0)
-	//d.AddLine(50.0, 100.0, 100.0, 100.0)
-	d.SaveImage("test")
-
+	d.SaveImage(name)
 }
 
-func buildConvexHull_2(points []Point) []Point {
+func buildConvexHull_2(points []Point) (result []Point, err error) {
 	sort.Sort(ByX(points))
-	currentPoint := points[0]
-	originPoint := points[0]
-	points = points[1:]
+	//fmt.Println("sorted ", points)
+	if len(points) < 3 {
+		log.Println(points)
+		return points, errors.New("Too few points.")
+	}
+	origin := points[0]
+	nextToOrigin := points[1]
+	currentPoint := origin
+	nextPoint := nextToOrigin
+	points = points[2:]
 
-	results := []Point{
-		originPoint,
+	result = []Point{
+		currentPoint,
+		nextPoint,
 	}
-	maxAngle := 0.0
-	var maxPos int
-	for {
-		for i, p := range points {
-			a := math.Atan((p.Y - currentPoint.Y) / (p.X - currentPoint.X))
-			if a > maxAngle {
-				maxAngle = a
-				maxPos = i
+	left := 0.0
+	for _, p := range points {
+		left = ccw(currentPoint, nextPoint, p)
+		if left < 0.0 {
+			l := len(result) - 1
+			if l == 1 { //only 2 elements in result, special treatment
+				result = append(result[:1], p)
+				nextPoint = p
+			} else {
+				for j := l; j >= 1; j-- {
+					if ccw(result[j-1], result[j], p) >= 0 {
+						result = append(result[:j+1], p)
+						//fmt.Println("new hull: ", result)
+						currentPoint = result[j]
+						nextPoint = p
+						break
+					}
+					if j == 1 {
+						result = append(result[:1], p)
+						nextPoint = p
+						currentPoint = result[0]
+					}
+				}
 			}
-		}
-		results = append(results, points[maxPos])
-		currentPoint = points[maxPos]
-		points = append(points[:maxPos], points[maxPos+1:]...)
-		if currentPoint.Equals(originPoint) {
-			break
+		} else {
+			result = append(result, p)
+			currentPoint = nextPoint
+			nextPoint = p
 		}
 	}
-	return results
+
+	currentPoint = origin
+	nextPoint = nextToOrigin
+	result_2 := []Point{
+		currentPoint,
+		nextPoint,
+	}
+
+	right := 0.0
+	for _, p := range points {
+		right = ccw(currentPoint, nextPoint, p)
+		if right > 0.0 {
+			l := len(result_2) - 1
+			if l == 1 { //only 2 elements in result, special treatment
+				result_2 = append(result_2[:1], p)
+				nextPoint = p
+			} else {
+				for j := l; j >= 1; j-- {
+					if ccw(result_2[j-1], result_2[j], p) <= 0 {
+						result_2 = append(result_2[:j+1], p)
+						//fmt.Println("new hull: ", result_2)
+						currentPoint = result_2[j]
+						nextPoint = p
+						break
+					}
+					if j == 1 {
+						result_2 = append(result_2[:1], p)
+						nextPoint = p
+						currentPoint = result_2[0]
+					}
+				}
+			}
+		} else {
+			result_2 = append(result_2, p)
+			currentPoint = nextPoint
+			nextPoint = p
+		}
+	}
+
+	for h := len(result_2) - 1; h > 0; h-- {
+		result = append(result, result_2[h])
+	}
+	result = append(result, origin)
+
+	return result, nil
+}
+
+/*(p1, p2, p3) is counterclockwise iff result > 0 => p3 is on the righthand side*/
+func ccw(p1, p2, p3 Point) float64 {
+	i := (p1.X-p2.X)*(p3.Y-p2.Y) - (p3.X-p2.X)*(p1.Y-p2.Y)
+	//fmt.Println(i)
+	return i
 }
 
 func buildConvexHull_1(points []Point) []Point {
@@ -151,13 +220,6 @@ func buildConvexHull_1(points []Point) []Point {
 				points = append(points[:i], points[i+1:]...)
 				fmt.Println("Added point... ", p)
 				results = append(results, p)
-
-				fmt.Println(a)
-				fmt.Println()
-				fmt.Println(c)
-				fmt.Println()
-				fmt.Println()
-
 				break
 			}
 		}
@@ -182,21 +244,42 @@ func (p *Point) Equals(q Point) bool {
 	return p.X == q.X && p.Y == q.Y
 }
 
-//d.PrintSomething()
-//TEST
-/*alpha := math.Pi/2 - 0.01
-a = math.Tan(alpha)
-c = currentPoint.Y + a*currentPoint.X
+/*points = []Point{
+	{100, 200},
+	{800, 200},
+	{600, 300},
+	{300, 500},
+	{700, 700},
+	{700, 100},
+	{400, 900},
+}
 
-fmt.Println(a)
-fmt.Println()
-fmt.Println(c)
-fmt.Println()
+result := []Point{
+	{100, 200},
+	{600, 300},
+	{300, 500},
+	{700, 700},
+	{400, 900},
+}
 
-for _, p := range points {
-	fmt.Println(distance(a, c, p))
+sort.Sort(ByX(points))
+sort.Sort(ByX(result))
 
-	if checkPoint(a, c, p) {
-		fmt.Println(p)
+fmt.Println(points)
+for i, p := range points {
+	for _, r := range result {
+		if p.X > r.X {
+			break
+		} else if p.Equals(r) {
+			if i == len(points)-1 {
+				points = points[:i]
+			} else if i == 0 {
+				points = points[1:]
+			} else {
+				points = append(points[:i], points[i+1:]...)
+			}
+		}
 	}
-}*/
+}
+fmt.Println(result)
+fmt.Println(points)*/
